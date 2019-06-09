16875d117a668ae75295423613ef7d212b3f05b9
karineSooma <karinesooma5411@gmail.com>
2019-06-09 19:32:06 -0300

    fazendo input do login p/ usuario de salao e cozinha

--- src/App.js
+++ src/App.js
@@ -1,21 +1,99 @@
 import React from 'react';
 import './App.css';
-import firebase from "./firebaseConfig";
+import firebase from './firebaseConfig';
+import Button from './components/Button';
+import withFirebaseAuth from 'react-with-firebase-auth';
+import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
+
+const firebaseAppAuth = firebase.auth();
+const database = firebase.firestore();
+
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      email:'',
+      password:'',
+      listUser:[]
+    };
   }
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1>#partiuBurgerQueen</h1>
-        </header>
-      </div>
-    );
+  componentDidMount() {
+    database.collection('users').get()
+      .then((querySnapshot) => {
+        const data = querySnapshot.docs.map(doc => doc.data());
+        this.setState({ listUser: data });
+        console.log(data);
+      });
   }
+
+  handleEmailChange = (event) => {
+    this.setState({email: event.target.value})
+  }
+
+  handlePasswordChange = (event) => {
+    this.setState({password: event.target.value})
+  }
+
+  handleClick = () => {
+    const object = {
+      email: this.state.email,
+        password: this.state.password
+    }
+    database.collection('users').add(object)
+    this.setState({
+      listUser: this.state.listUser.concat(object)
+    })
+  }
+
+createUser = () => {
+  this.props.createUserWithEmailAndPassword(this.state.email, this.state.password)
 }
 
-export default App;
+signIn = () => {
+  this.props.signInWithEmailAndPassword(this.state.email, this.state.password)
+    .then(() => {
+      alert('uhuu');
+    });
+}
+<Router>
+<Route path='/' Component{Index}/>
+<Route path='/salao' Component={Salao}/>
+</Router>
+
+function Index() {
+  return (
+    <p>Sooooo</p>
+  );
+}
+
+function Salao() {
+  return (
+    <p>Salaaao</p>
+  );
+}
+  render() {
+    return (
+    <div className="App">
+        <header className="App-header">
+          <h1>Burger-Queen</h1>
+          <input value={this.state.email} 
+          placeholder="Digite seu email"
+          onChange={this.handleEmailChange}/>
+           <input value={this.state.password}
+          placeholder="Digite sua senha"
+          onChange={this.handlePasswordChange}/>
+          <Button text='criar usuário' onClick={this.createUser} />
+          <Button text='Login' onClick={this.signIn} />
+        {
+          this.state.listUser.map((item, index) => {
+            return <p key={index}>{item.email} | {item.password}</p>
+          })
+        }
+        </header>
+        </div>
+      )
+    }
+  }
+
+export default withFirebaseAuth({firebaseAppAuth})(App);
--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const config = {
-  apiKey: "<sua-chave>",
-  authDomain: "<sua-chave>",
-  databaseURL: "<sua-chave>",
-  projectId: "<sua-chave>",
-  storageBucket: "<sua-chave>",
-  messagingSenderId: "<sua-chave>",
-  appId: "<sua-chave>"
+  apiKey: "AIzaSyAEdad3eLq4TwTAzWJy5GuICuG4HGWvIOE",
+  authDomain: "burger-queen-ks.firebaseapp.com",
+  databaseURL: "https://burger-queen-ks.firebaseio.com",
+  projectId: "burger-queen-ks",
+  storageBucket: "burger-queen-ks.appspot.com",
+  messagingSenderId: "203511956132",
+  appId: "1:203511956132:web:28ee37356d5a14d6"
 };
 
 firebase.initializeApp(config);
